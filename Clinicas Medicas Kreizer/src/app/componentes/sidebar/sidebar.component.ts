dcb0ada973a302e36baf887629b8f0a471c88ed8
wes19 <58017143+wes19@users.noreply.github.com>
2023-10-30 23:42:27 -0600

    Avance 1 30/10/23

--- Clinicas Medicas Kreizer/src/app/componentes/sidebar/sidebar.component.ts
+++ Clinicas Medicas Kreizer/src/app/componentes/sidebar/sidebar.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
 import { SidebarService } from './sidebar.service';
 
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent  {
+  selectedOption: string | null = null;
+  imagePath: string = '../assets/img/logoLetter.svg';
+
   constructor(private sidebarService: SidebarService) {}
 
+  updateImage() {
+    const isSidebarOpen = this.getSidebarState();
+    if (isSidebarOpen) {
+      this.imagePath = '../assets/img/logoLetter.svg';
+    } else {
+      this.imagePath = '../assets/img/logo.svg';
+    }
+  }
+  
   toggleSidebar() {
     this.sidebarService.toggleSidebar();
+    this.updateImage();
   }
 
   getSidebarState() {
     return this.sidebarService.getSidebarState();
   }
-
- selectedOption: string | null = null;
 
   onOptionClick(option: string) {
     if (this.selectedOption === option) {
